6aad536c326902e318de6369384ae5741b94dfd1
Esteban Meza <meza.19@osu.edu>
2021-03-18 00:00:12 -0400

    polished and cleaned the code

--- app.js
+++ app.js
@@ -87,13 +87,10 @@ function addEmployee() {
             choices:["Manager", "Engineer", "Intern", "Finish"]
         },
     ];
-
+    // Depending on the character chosen, performs function to ask specific questions
     inquirer.prompt(firstQuestion).then(function(data) {
-        console.log("the choice is " + data.addRoles);
         const employeeType = data.addRoles;
-        console.log("Employee Type is " + employeeType);
         if (employeeType === "Manager") {
-            console.log("part 1 succedded")
             managerQuestions();
         }
         else if (employeeType === "Engineer") {
@@ -103,77 +100,44 @@ function addEmployee() {
             internQuestions();
         }
         else {
-            console.log("Attempting yeet");
             fs.writeFileSync(outputPath, render(team), function(err) {
                 if (err){
                     console.log(err)
                 }
-                else {
-                    console.log("Output complete.")
-                }
-                console.log("Yeeted")
+                else console.log("complete");
             });
         }
     })
 };
 // functions to return data for each type of employee
 function managerQuestions () {
-    console.log("Part 2 succedded")
     inquirer.prompt(mQuestions).then((mData) => {
         let newManager = new Manager(mData.name, mData.id, mData.email, mData.office);
         team.push(newManager);
         addEmployee();
     }
     );
-    
 };
+//inquier prompt for engineer
 function engineerQuestions() {
-    console.log("add engineeer started!");
     inquirer.prompt(eQuestions).then((eData) => {
         let newEngineer = new Engineer(eData.name, eData.id, eData.email, eData.github);
         team.push(newEngineer);
         addEmployee();
     }
     );
-    
 }
+// inquier prompt for intern
 function internQuestions() {
-    console.log("attempting intern question");
     inquirer.prompt(iQuestions).then((iData) => {
         let newIntern = new Intern(iData.name, iData.id, iData.email, iData.school);
         team.push(newIntern);
         addEmployee();
     }
     );
-    
 }
-// Until the user decides to quit, this will continue to run
-
-    console.log("begin");
+    // Begins function to add employee
     addEmployee();
-    console.log("Finalizing YEET");
 
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
